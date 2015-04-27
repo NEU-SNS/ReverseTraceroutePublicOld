0cbcf5d2f7583190492c269b624775fbf1f1993b
Rob <rhansen2@buffalo.edu>
2015-04-27 17:00:32 -0400

    renamed file and added types to plvp

--- lib/plvp/plvantagepoint.go
+++ lib/plvp/plvantagepoint.go
@@ -1,7 +1,7 @@
 /*
  Copyright (c) 2015, Northeastern University
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
      * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
      * Neither the name of the Northeastern University nor the
        names of its contributors may be used to endorse or promote products
        derived from this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -24,13 +24,18 @@
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-package vp
+package plvp
 
-type Vantagepoint struct {
+import (
+	"github.com/NEU-SNS/ReverseTraceroute/lib/scamper"
+)
+
+type PLVantagepoint struct {
 	version  int
 	ip       string
 	port     int
 	hostname string
 	socket   string
 	canspoof bool
+	sc       scamper.ScamperConfig
 }
