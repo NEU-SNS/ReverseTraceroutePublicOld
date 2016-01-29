53860a00aa3d857273034caa87623b5c03cd661b
Rob Hansen <rob.hansen320@gmail.com>
2016-01-29 18:48:02 -0500

    work

---
+++ scripts/build/buildPLVP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../../cmd/plvp
+
+GOOS=linux GOARCH=386 go build -ldflags \
+    "-X main.versionNo=$1 -X main.pidFile=$2 -X main.lockFile=$3 -X main.configPath=/home/uw_geoloc4/plvp/plvp.config"
