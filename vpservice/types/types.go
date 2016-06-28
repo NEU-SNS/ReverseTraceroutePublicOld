7f22045880ecfba237f1136552086b56df1602d6
Rob Hansen <rob.hansen320@gmail.com>
2016-06-28 14:17:53 -0400

    Add dataacces and new types for vpservice quarantine

--- vpservice/types/types.go
+++ vpservice/types/types.go
@@ -28,6 +28,10 @@ Copyright (c) 2015, Northeastern University
 package types
 
 import (
+	"encoding/json"
+	"fmt"
+	"math"
+	"math/rand"
 	"time"
 
 	"github.com/NEU-SNS/ReverseTraceroute/vpservice/pb"
@@ -44,6 +48,8 @@ type VPProvider interface {
 	UnquarantineVPs(vps []Quarantine) error
 	QuarantineVPs(vps []Quarantine) error
 	GetQuarantined() ([]Quarantine, error)
+	GetLastQuarantine(uint32) (Quarantine, error)
+	UpdateQuarantines([]Quarantine) error
 }
 
 // QuarantineReason is a reason to quarantine a vp
@@ -73,11 +79,27 @@ type Quarantine interface {
 	GetLastAttempt() time.Time
 	GetBackoff() time.Time
 	GetInitialBackoff() time.Duration
+	SetInitialBackoff(time.Duration)
 	GetMultiplier() int
 	GetMaxBackoff() time.Duration
 	GetNextInitialBackoff() time.Duration
+	GetCurrentBackoff() time.Duration
 	GetExpire() time.Time
+	NextBackoff() time.Time
+	Expired(time.Time) bool
+	Elapsed(time.Time) bool
+	Type() QuarantineType
 }
+
+// QuarantineType identifies the quarantine type
+type QuarantineType string
+
+const (
+	// DefaultQuar is the default Quarantine
+	DefaultQuar QuarantineType = "DEFAULT"
+	// ManualQuar is a manual quarantine
+	ManualQuar QuarantineType = "MANUAL"
+)
 
 type defaultQuarantine struct {
 	VP                 pb.VantagePoint  `json:"vp"`
@@ -91,57 +113,133 @@ type defaultQuarantine struct {
 	MaxBackoff         time.Duration    `json:"max_backoff"`
 	NextInitialBackoff time.Duration    `json:"next_init_backoff"`
 	Expire             time.Time        `json:"expire"`
+	CurrentBackoff     time.Duration    `json:"CurrentBackoff"`
 }
 
 func (dq *defaultQuarantine) GetVP() pb.VantagePoint {
 	return dq.VP
 }
+
 func (dq *defaultQuarantine) GetReason() QuarantineReason {
 	return dq.Reason
 }
+
 func (dq *defaultQuarantine) GetAttempt() int {
 	return dq.Attempt
 }
+
 func (dq *defaultQuarantine) GetAdded() time.Time {
 	return dq.Added
 }
+
 func (dq *defaultQuarantine) GetLastAttempt() time.Time {
 	return dq.LastAttempt
 }
+
 func (dq *defaultQuarantine) GetBackoff() time.Time {
 	return dq.Backoff
 }
+
 func (dq *defaultQuarantine) GetInitialBackoff() time.Duration {
 	return dq.InitialBackoff
 }
+
+func (dq *defaultQuarantine) SetInitialBackoff(ib time.Duration) {
+	dq.InitialBackoff = ib
+}
+
 func (dq *defaultQuarantine) GetMultiplier() int {
 	return dq.Multiplier
 }
+
 func (dq *defaultQuarantine) GetMaxBackoff() time.Duration {
 	return dq.MaxBackoff
 }
+
 func (dq *defaultQuarantine) GetNextInitialBackoff() time.Duration {
 	return dq.NextInitialBackoff
 }
+
 func (dq *defaultQuarantine) GetExpire() time.Time {
 	return dq.Expire
+}
+
+func (dq *defaultQuarantine) NextBackoff() time.Time {
+	dq.Attempt++
+	back := dq.InitialBackoff * time.Duration(power(dq.Multiplier, dq.Attempt))
+	var maxBack time.Duration
+	if back > dq.MaxBackoff {
+		maxBack = dq.MaxBackoff
+	} else {
+		maxBack = back
+	}
+	dq.CurrentBackoff = time.Duration(rand.Int63n(int64(maxBack))) + dq.InitialBackoff
+	dq.Backoff = time.Now().Add(dq.CurrentBackoff)
+	return dq.GetBackoff()
+}
+
+func (dq *defaultQuarantine) GetCurrentBackoff() time.Duration {
+	return dq.CurrentBackoff
+}
+
+func (dq *defaultQuarantine) Expired(now time.Time) bool {
+	if dq.Expire.IsZero() {
+		return false
+	}
+	if dq.Expire.After(now) || dq.Expire.Equal(now) {
+		return true
+	}
+	return false
+}
+
+func (dq *defaultQuarantine) Elapsed(now time.Time) bool {
+	if dq.Backoff.After(now) || dq.Backoff.Equal(now) {
+		return true
+	}
+	return false
+}
+
+func (dq *defaultQuarantine) Type() QuarantineType {
+	return DefaultQuar
+}
+
+// NewDefaultQuarantine creates a defaultQuarantine
+func NewDefaultQuarantine(vp pb.VantagePoint, prevQuar Quarantine) Quarantine {
+	var q defaultQuarantine
+	q.InitialBackoff = time.Hour * 24
+	q.Multiplier = 2
+	q.MaxBackoff = time.Hour * 24 * 7
+	q.NextInitialBackoff = time.Hour * 24
+	if prevQuar != nil {
+		// If there is a previous quarantine of a different type we use default settings
+		// If they are the same type we use the smaller of the two backoffs
+		if prevQuar.Type() == DefaultQuar {
+			if prevQuar.GetCurrentBackoff() < q.InitialBackoff {
+				q.InitialBackoff = prevQuar.GetCurrentBackoff()
+			}
+		}
+	}
+	return &q
 }
 
 type manualQuarantine struct {
 	defaultQuarantine
 }
 
-// NewDefaultQuarantine creates a defaultQuarantine
-func NewDefaultQuarantine(vp pb.VantagePoint, prevQuar bool) Quarantine {
-	var q defaultQuarantine
-	q.InitialBackoff = time.Hour * 24
-	q.Multiplier = 2
-	q.MaxBackoff = time.Hour * 24 * 7
-	q.NextInitialBackoff = time.Hour * 24
-	if prevQuar {
-		q.InitialBackoff = q.GetNextInitialBackoff()
-	}
-	return &q
+func (mq *manualQuarantine) Type() QuarantineType {
+	return ManualQuar
+}
+
+func (mq *manualQuarantine) Elapsed(now time.Time) bool {
+	// Always return false, manual quarantines do not
+	// elapse, they only exipire
+	return false
+}
+func (mq *manualQuarantine) NextBackoff() time.Time {
+	// Manual Quarantines do not backoff, they are just
+	// absolute times which the quarantine expires
+	// Just return the expire time for now
+	return mq.Expire
 }
 
 // NewManualQuarantine creates a manualQuarantine with expire exp
@@ -149,6 +247,32 @@ func NewManualQuarantine(vp pb.VantagePoint, exp time.Time) Quarantine {
 	q := manualQuarantine{}
 	q.Expire = exp
 	return &q
+}
+
+// GetQuarantine converts the data given into a quarantine of the given type
+func GetQuarantine(t QuarantineType, data []byte) (Quarantine, error) {
+	switch t {
+	case DefaultQuar:
+		q := &defaultQuarantine{}
+		err := json.Unmarshal(data, q)
+		if err != nil {
+			return nil, err
+		}
+		return q, nil
+	case ManualQuar:
+		q := &manualQuarantine{}
+		err := json.Unmarshal(data, q)
+		if err != nil {
+			return nil, err
+		}
+		return q, nil
+	default:
+		return nil, fmt.Errorf("Unknown QuarantineType: %v", t)
+	}
+}
+
+func power(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
 }
 
 // RRVantagePoint represents a vantage point
